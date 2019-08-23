7e584472375b4fec644aa11b579d93ea2d42215b
Stefan Volz <volzstefan97@googlemail.com>
2019-08-23 23:24:18 +0200

    added f32 implementation

--- rtc/src/approx_eq.rs
+++ rtc/src/approx_eq.rs
@@ -12,11 +12,24 @@ impl ApproxEq for f64 {
     }
 }
 
+impl ApproxEq for f32 {
+    const EPSILON: Self = 0.1e-5;
+    fn approx_eq(self, other: Self) -> bool {
+        (self - other).abs() < Self::EPSILON
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn approx_eq_f64() {
+        assert!(4.2.approx_eq(4.2000000000000001));
+        assert!(!1.2.approx_eq(1.1));
+    }
+
+    #[test]
+    fn approx_eq_f32() {
         assert!(4.2.approx_eq(4.2000000000000001));
         assert!(!1.2.approx_eq(1.1));
     }
