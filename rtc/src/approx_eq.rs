685bfaade947d56c5119f116606e553254f219ab
Stefan Volz <volzstefan97@googlemail.com>
2019-08-23 18:38:48 +0200

    Trait for approximate equality tests

---
+++ rtc/src/approx_eq.rs
@@ -0,0 +1,23 @@
+/// Trait that provides equivalence for floating-point based types
+pub trait ApproxEq {
+    /// Maximum allowed error such that two instances are regarded as being equal.
+    const EPSILON: Self;
+    fn approx_eq(self, other: Self) -> bool;
+}
+
+impl ApproxEq for f64 {
+    const EPSILON: Self = 0.1e-10;
+    fn approx_eq(self, other: Self) -> bool {
+        (self - other).abs() < Self::EPSILON
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn approx_eq_f64() {
+        assert!(4.2.approx_eq(4.2000000000000001));
+        assert!(!1.2.approx_eq(1.1));
+    }
+}
