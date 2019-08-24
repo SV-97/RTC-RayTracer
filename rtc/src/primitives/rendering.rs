d7f4ba02253be4f24fac1af51af9168f8b382f87
Stefan Volz <volzstefan97@googlemail.com>
2019-08-24 15:50:55 +0200

    New struct to hold a single rendering and save it to file

---
+++ rtc/src/primitives/rendering.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use super::canvas::Canvas;
+
+pub struct Rendering {
+    name: String,
+    canvas: Canvas,
+}
+
+impl Rendering {
+    pub fn new(name: impl Into<String>, canvas: Canvas) -> Self {
+        Rendering { 
+            name: name.into(),
+            canvas
+        }
+    }
+
+    pub fn save_to_file(&self) -> std::io::Result<()> {
+        let mut file = File::create(&format!("renders/{}.ppm", self.name))?;
+        file.write_all(self.canvas.as_ppm().as_bytes())?;
+        Ok(())
+    }
+}
