9332194f76be05c237c93f725fce6791e4fdbbaa
Stefan Volz <volzstefan97@googlemail.com>
2019-08-26 15:09:36 +0200

    added identity matrices and submatrices

--- rtc/src/primitives/tmatrix.rs
+++ rtc/src/primitives/tmatrix.rs
@@ -1,8 +1,8 @@
-use num_traits::Num;
+use num_traits::{Num, One, Zero};
 
 use std::fmt;
-use std::ops::{Index, IndexMut, Mul};
 use std::marker::PhantomData;
+use std::ops::{Index, IndexMut, Mul};
 
 use crate::utils::typelevel_nums::*;
 
@@ -20,7 +20,6 @@ pub struct Matrix<T, M: Nat, N: Nat> {
     _n: PhantomData<N>,
     pub data: Vec<T>,
 }
-
 
 /// Create a new matrix of specified size
 /// Verifies that the number of values matches the size
@@ -131,7 +130,6 @@ impl<T, M: Nat + Val, N: Nat + Val> Matrix<T, M, N> {
     }
 }
 
-
 impl<T, M: Nat + Val, N: Nat + Val> fmt::Debug for Matrix<T, M, N>
 where
     T: fmt::Debug + Default + Clone,
@@ -194,6 +192,101 @@ where
             }
         }
         new
+    }
+}
+
+impl<T, M> Matrix<T, M, M>
+where
+    T: Clone + One + Zero,
+    M: Nat + Val,
+{
+    pub fn identity() -> Self {
+        let mut m = Matrix::new_uninitialized();
+        m.data = vec![T::zero(); M::val() * M::val()];
+        for i in 0..M::val() {
+            m[(i, i)] = T::one();
+        }
+        m
+    }
+}
+
+impl<T, M, N> Matrix<T, Succ<M>, N>
+where
+    T: Copy,
+    M: Nat + Val,
+    N: Nat + Val,
+{
+    /// Remove the i'th row
+    pub fn remove_row(&self, i: usize) -> Matrix<T, M, N> {
+        let mut m = Matrix::new_uninitialized();
+        m.data = self
+            .iter_rows()
+            .enumerate()
+            .filter_map(|(x, row)| if x != i { Some(row) } else { None })
+            .flatten()
+            .copied()
+            .collect::<Vec<T>>();
+        m
+    }
+}
+
+impl<T, M, N> Matrix<T, M, Succ<N>>
+where
+    T: Copy,
+    M: Nat + Val,
+    N: Nat + Val,
+{
+    /// Remove the i'th row
+    pub fn remove_col(&self, j: usize) -> Matrix<T, M, N> {
+        let mut m = Matrix::new_uninitialized();
+        m.data = self
+            .iter_cols()
+            .enumerate()
+            .filter_map(|(x, col)| if x != j { Some(col) } else { None })
+            .fold(None, |old: Option<Vec<Vec<_>>>, new| match old {
+                // zip all the iterators
+                // TODO Maybe test another datastructure here - VecDeque or LinkedList or smth
+                // Could also transpose -> remove row -> transpose, if that is more efficient
+                Some(it) => Some(
+                    it.into_iter()
+                        .zip(new)
+                        .map(|(mut v, elem)| {
+                            v.push(elem);
+                            v
+                        })
+                        .collect::<Vec<Vec<_>>>(),
+                ),
+                None => Some(new.map(|x| vec![x]).collect::<Vec<_>>()),
+            })
+            .unwrap()
+            .into_iter()
+            .flatten()
+            .copied()
+            .collect::<Vec<T>>();
+        m
+    }
+}
+
+impl<T, M, N> Matrix<T, Succ<M>, Succ<N>>
+where
+    T: Copy,
+    M: Nat + Val,
+    N: Nat + Val,
+    Succ<M>: Nat + Val,
+    Succ<N>: Nat + Val,
+{
+    pub fn submatrix(&self, i: usize, j: usize) -> Matrix<T, M, N> {
+        self.remove_row(i).remove_col(j)
+    }
+}
+
+impl<T: Num + Copy> Matrix<T, N2, N2> {
+    /// Calculate determinant of a 2x2 matrix.
+    pub fn det(&self) -> T {
+        match self.data[..] {
+            [a, b, c, d] => a * d - b * c,
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -342,5 +435,95 @@ mod tests {
             40 58 110 102
         ];
         assert_eq!(a * b, c);
+    }
+
+    #[test]
+    fn identity() {
+        let a = matrix![ N3, N3 =>
+            1 2 3;
+            4 5 6;
+            7 8 9
+        ];
+        assert_eq!(a.clone() * <Matrix<_, N3, N3>>::identity(), a.clone());
+    }
+
+    #[test]
+    fn det() {
+        let a = matrix![ N2, N2 =>
+            1 5 ;
+            {-3} 2
+        ];
+        assert_eq!(a.det(), 17);
+    }
+
+    #[test]
+    fn remove_row() {
+        let a = matrix![ N5, N5 =>
+            1 5 3 4 5;
+            2 2 7 9 8;
+            3 2 1 7 9;
+            5 5 3 6 1;
+            0 5 0 2 4
+        ];
+        assert_eq!(
+            a.remove_row(0),
+            matrix![ N4, N5 =>
+                2 2 7 9 8;
+                3 2 1 7 9;
+                5 5 3 6 1;
+                0 5 0 2 4
+            ]
+        );
+    }
+
+    #[test]
+    fn remove_col() {
+        let a = matrix![ N5, N5 =>
+            1 5 3 4 5;
+            2 2 7 9 8;
+            3 2 1 7 9;
+            5 5 3 6 1;
+            0 5 0 2 4
+        ];
+        assert_eq!(
+            a.remove_col(2),
+            matrix![ N5, N4 =>
+                1 5 4 5;
+                2 2 9 8;
+                3 2 7 9;
+                5 5 6 1;
+                0 5 2 4
+            ]
+        );
+    }
+
+    #[test]
+    fn submatrix() {
+        let a = matrix![ N3, N3 =>
+            1, 5, 0;
+            -3, 2, 2;
+            0, 6, -3
+        ];
+        assert_eq!(
+            a.submatrix(0, 2),
+            matrix![ N2, N2 =>
+                -3, 2;
+                0, 6
+            ]
+        );
+        let b = matrix![ N4, N4 =>
+            -6, 1, 1, 6;
+            -8, 5, 8, 6;
+            -1, 0, 8, 2;
+            -7, 1, -1, 1
+        ];
+        assert_eq!(
+            b.submatrix(2, 1),
+            matrix![ N3, N3 =>
+                -6, 1, 6;
+                -8, 8, 6;
+                -7, -1, 1
+            ]
+        );
     }
 }
