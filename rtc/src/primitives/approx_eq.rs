c6d3104fd27eb0792d45f9c3a1c65b28a7c7aa49
Stefan Volz <volzstefan97@googlemail.com>
2019-08-28 14:24:35 +0200

    Removed Copy trait bound on ApproxEq and added macro for assertions

--- rtc/src/primitives/approx_eq.rs
+++ rtc/src/primitives/approx_eq.rs
@@ -1,8 +1,5 @@
 /// Trait that provides equivalence for floating-point based types
-pub trait ApproxEq<E = Self, Rhs = Self>
-where
-    E: Copy,
-{
+pub trait ApproxEq<E = Self, Rhs = Self> {
     /// Maximum allowed error such that two instances are regarded as being equal.
     const EPSILON: E;
     fn approx_eq(self, other: Rhs) -> bool;
@@ -22,6 +19,64 @@ impl ApproxEq for f32 {
     }
 }
 
+/*
+impl<E> ApproxEq<E> for Vec<E>
+where E: ApproxEq
+{
+    const EPSILON: E = E::EPSILON;
+    fn approx_eq(self, other: Self) -> bool {
+        self.iter().zip(other.iter()).all(|(l, r)| l.approx_eq(*r))
+    }
+}
+
+impl<'a, E> ApproxEq<E> for &'a Vec<E>
+where
+    &'a E: ApproxEq,
+    E: ApproxEq
+{
+    const EPSILON: E = E::EPSILON;
+    fn approx_eq(self, other: Self) -> bool {
+        self.iter().zip(other.iter()).all(|(l, r)| l.approx_eq(r))
+    }
+}
+*/
+
+/// Adaption of assert_eq from the stdlib to work with assert_eq rather than std::ops::Eq::eq
+#[macro_export]
+macro_rules! assert_approx_eq {
+    ($left:expr, $right:expr) => ({
+        use $crate::primitives::approx_eq::ApproxEq;
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if !((left_val).approx_eq(*right_val)) {
+                    // The reborrows below are intentional. Without them, the stack slot for the
+                    // borrow is initialized even before the values are compared, leading to a
+                    // noticeable slow down.
+                    panic!(r#"assertion failed: `(left ≈ right)`
+  left: `{:?}`,
+ right: `{:?}`"#, &*left_val, &*right_val)
+                }
+            }
+        }
+    });
+    ($left:expr, $right:expr, $($arg:tt)+) => ({
+        use $crate::primitives::approx_eq::ApproxEq;
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if !((left_val).approx_eq(*right_val)) {
+                    // The reborrows below are intentional. Without them, the stack slot for the
+                    // borrow is initialized even before the values are compared, leading to a
+                    // noticeable slow down.
+                    panic!(r#"assertion failed: `(left ≈ right)`
+  left: `{:?}`,
+ right: `{:?}`: {}"#, &*left_val, &*right_val,
+                           format_args!($($arg)+))
+                }
+            }
+        }
+    });
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +90,16 @@ mod tests {
     fn approx_eq_f32() {
         assert!(4.2.approx_eq(4.2000000000000001));
         assert!(!1.2.approx_eq(1.1));
+    }
+
+    #[test]
+    #[should_panic]
+    fn approx_eq_macro_panic() {
+        assert_approx_eq!(4.2, 4.3);
+    }
+
+    #[test]
+    fn approx_eq_macro() {
+        assert_approx_eq!(4.2, 4.2000000000000001);
     }
 }
